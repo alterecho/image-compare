6fbb22ab3d517afe4b6ff8843b8fd06f2b60a855
vijay <vjc@gmail.com>
2024-11-24 00:09:48 +0800

    applied cell structure (style)

--- src/common/components/MetaDataTable/MetaDataCell.js
+++ src/common/components/MetaDataTable/MetaDataCell.js
@@ -1,12 +1,34 @@
 import { View, Text, TouchableOpacity } from "react-native";
+import { StyleSheet } from "react-native";
 
 const MetaDataCell = ({ metaData, onSelectMetaData }) => {
   return (
-    <TouchableOpacity onPress={() => onSelectMetaData(metaData)}>
-      <Text>{metaData.title}</Text>
-      <Text>{metaData.value}</Text>
+    <TouchableOpacity style={styles.cell} onPress={() => onSelectMetaData(metaData)}>
+      <Text style={ styles.title }>{metaData.title}</Text>
+      <Text style={ styles.value }>{metaData.value}</Text>
     </TouchableOpacity >
   )
 }
+
+const styles = StyleSheet.create(
+  {
+    cell: {
+      flex: 1,
+      flexDirection: 'row',
+      alignItems:'center'
+    },
+    title: {
+      flex: 0.45,
+      textAlign: 'right',
+      paddingRight: 8
+    },
+    value: {
+      flex: 0.55,
+      textAlign: 'left',
+      paddingLeft: 8
+    }
+
+  }
+)
 
 export default MetaDataCell;
