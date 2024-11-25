4c5e3f83311b58da7d969824ac76391322934eeb
vijay <vjc@gmail.com>
2024-11-25 18:14:24 +0800

    Use theme in APp.js

--- src/App.js
+++ src/App.js
@@ -6,19 +6,23 @@ import CompareImageScreen from './screens/CompareImageScreen/CompareImageScreen'
 import CompareMetaDataScreen from './screens/CompareMetaDataScreen/CompareMetaDataScreen';
 import MetaDataScreen from './screens/MetaDataScreen/MetaDataScreen';
 import { Pages } from './screens/Constants';
+import Theme from './Theme';
 
 const App = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false)
   const Stack = createNativeStackNavigator();
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName={Pages.COMPARE_IMAGE_SCREEN}
-        screenOptions={{ headerShown: false }}>
-        <Stack.Screen name={Pages.COMPARE_IMAGE_SCREEN} component={CompareImageScreen} />
-        <Stack.Screen name={Pages.COMPARE_META_DATA_PAGE} component={CompareMetaDataScreen} />
-        <Stack.Screen name={Pages.META_DATA_PAGE} component={MetaDataScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Theme.context.Provider value={{ theme: isDarkMode ? Theme.darkTheme : Theme.lightTheme , toggleTheme: () => setIsDarkMode(!isDarkMode) }}>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName={Pages.COMPARE_IMAGE_SCREEN}
+          screenOptions={{ headerShown: false }}>
+          <Stack.Screen name={Pages.COMPARE_IMAGE_SCREEN} component={CompareImageScreen} />
+          <Stack.Screen name={Pages.COMPARE_META_DATA_PAGE} component={CompareMetaDataScreen} />
+          <Stack.Screen name={Pages.META_DATA_PAGE} component={MetaDataScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Theme.context.Provider>
   );
 };
 
