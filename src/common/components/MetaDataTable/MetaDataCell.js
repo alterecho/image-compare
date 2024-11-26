7f62982713f9e22e32099fec834d4ca5acfbb2bb
vijay <vjc@gmail.com>
2024-11-26 10:14:58 +0800

    theme metaDataCell

--- src/common/components/MetaDataTable/MetaDataCell.js
+++ src/common/components/MetaDataTable/MetaDataCell.js
@@ -1,34 +1,44 @@
 import { View, Text, TouchableOpacity } from "react-native";
 import { StyleSheet } from "react-native";
+import Theme from "../../../Theme";
+import { useContext } from "react";
 
 const MetaDataCell = ({ metaData, onSelectMetaData }) => {
+  const theme = useContext(Theme.context)
+  const styles = makeStyleSheet(theme)
+
   return (
-    <TouchableOpacity style={styles.cell} onPress={() => onSelectMetaData(metaData)}>
-      <Text style={ styles.title }>{metaData.title}</Text>
-      <Text style={ styles.value }>{metaData.value}</Text>
+    <TouchableOpacity style={styles.container} onPress={() => onSelectMetaData(metaData)}>
+      <Text style={styles.title}>{metaData.title}</Text>
+      <Text style={styles.value}>{metaData.value}</Text>
     </TouchableOpacity >
   )
 }
 
-const styles = StyleSheet.create(
-  {
-    cell: {
-      flex: 1,
-      flexDirection: 'row',
-      alignItems:'center'
-    },
-    title: {
-      flex: 0.45,
-      textAlign: 'right',
-      paddingRight: 8
-    },
-    value: {
-      flex: 0.55,
-      textAlign: 'left',
-      paddingLeft: 8
+const makeStyleSheet = (theme) => {
+  return StyleSheet.create(
+    {
+      container: {
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center',
+        backgroundColor: 'clear'
+      },
+      title: {
+        color: theme.primaryColor,
+        flex: 0.45,
+        textAlign: 'right',
+        paddingRight: 8,
+        backgroundColor: 'clear'
+      },
+      value: {
+        color: theme.primaryColor,
+        flex: 0.55,
+        textAlign: 'left',
+        paddingLeft: 8
+      }
     }
-
-  }
-)
+  );
+}
 
 export default MetaDataCell;
