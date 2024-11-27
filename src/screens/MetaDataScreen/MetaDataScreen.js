aa27f1e22c12fbfe3f8631a6436199109c92c895
vijay <vjc@gmail.com>
2024-11-27 15:12:27 +0800

    CompareMetaDataScreen, MetaDataScreen SafeAreaView from "react-native-safe-area-context";

--- src/screens/MetaDataScreen/MetaDataScreen.js
+++ src/screens/MetaDataScreen/MetaDataScreen.js
@@ -1,17 +1,19 @@
-import { SafeAreaView } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import { MetaDataTableView } from "../../common/components/MetaDataTable/MetaDataTableView";
 
 const MetaDataScreen = ({ route }) => {
     const metaDataArray = route.params.metaDataArray
     return (
-        <SafeAreaView
-            style={{
-                flex: 1,
-                flexDirection: 'column',
-                backgroundColor: 'green'
-            }}>
-            <MetaDataTableView style={{ flex: 1 }} metaDataArray={metaDataArray} />
-        </SafeAreaView>
+        <SafeAreaProvider>
+            <SafeAreaView
+                style={{
+                    flex: 1,
+                    flexDirection: 'column',
+                    backgroundColor: 'green'
+                }}>
+                <MetaDataTableView style={{ flex: 1 }} metaDataArray={metaDataArray} />
+            </SafeAreaView>
+        </SafeAreaProvider>
     )
 }
 
