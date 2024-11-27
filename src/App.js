2d50c7174ea36ac178a255ef8a4ea492fafcd1b4
vijay <vjc@gmail.com>
2024-11-27 15:49:14 +0800

    headerView targeting per screen

--- src/App.js
+++ src/App.js
@@ -11,14 +11,25 @@ const App = () => {
   const [isDarkMode, setIsDarkMode] = useState(false)
   const Stack = createNativeStackNavigator();
   return (
-    <Theme.context.Provider value={{ theme: isDarkMode ? Theme.darkTheme : Theme.lightTheme , toggleTheme: () => setIsDarkMode(!isDarkMode) }}>
+    <Theme.context.Provider value={{ theme: isDarkMode ? Theme.darkTheme : Theme.lightTheme, toggleTheme: () => setIsDarkMode(!isDarkMode) }}>
       <NavigationContainer>
         <Stack.Navigator
           initialRouteName={Pages.COMPARE_IMAGE_SCREEN}
           screenOptions={{ headerShown: false }}>
-          <Stack.Screen name={Pages.COMPARE_IMAGE_SCREEN} component={CompareImageScreen} />
-          <Stack.Screen name={Pages.COMPARE_META_DATA_PAGE} component={CompareMetaDataScreen} />
-          <Stack.Screen name={Pages.META_DATA_PAGE} component={MetaDataScreen} />
+          <Stack.Screen
+            name={Pages.COMPARE_IMAGE_SCREEN}
+            component={CompareImageScreen}
+          />
+          <Stack.Screen
+            name={Pages.COMPARE_META_DATA_PAGE}
+            component={CompareMetaDataScreen}
+            screenOptions={{ headerShown: true }}
+          />
+          <Stack.Screen
+            name={Pages.META_DATA_PAGE}
+            component={MetaDataScreen}
+            screenOptions={{ headerShown: true }}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </Theme.context.Provider>
