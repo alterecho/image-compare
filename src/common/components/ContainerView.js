6ecbcfbdb8505967009acc2c93567130a02419d4
vijay <vjc@gmail.com>
2024-12-05 00:18:47 +0800

    WIP scroll corresponding view

--- src/common/components/ContainerView.js
+++ src/common/components/ContainerView.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useRef, useState } from "react";
+import React, { forwardRef, useContext, useEffect, useImperativeHandle, useRef, useState } from "react";
 import { View, Image, Button, StyleSheet, useAnimatedValue } from "react-native";
 import * as Utils from "../../common/utilities/Utils";
 import Toolbar from "./Toolbar";
@@ -12,164 +12,189 @@ import MetaDataView from "./MetaDataView";
 import TestMetaData from "../../../tests/data/metadata-sample1.json"
 import { Config as ToolbarConfig } from "./Toolbar";
 
-const ContainerView = ({ imageInfo, onPressAddPictureButton }) => {
-  const [imageSize, setImageSize] = useState(Size(0, 0));
-  const translateX = useSharedValue(0);
-  const translateY = useSharedValue(0);
-  const scale = useSharedValue(1.0);
-  const startX = useSharedValue(0);
-  const startY = useSharedValue(0);
-  const startScale = useSharedValue(0);
-  const { theme, toggleTheme } = useContext(Theme.context)
-  const styles = makeStyleSheet(theme)
-  const [viewSize, setViewSize] = useState(Size(0, 0))
-  const [isShowMetaData, setIsShowMetaData] = useState(false);
+const ContainerView = forwardRef(
+  ({ imageInfo, onPressAddPictureButton, onSelectMetaDataItemHandler }, ref) => {
+    const metaDataViewRef = useRef(null)
+    const [imageSize, setImageSize] = useState(Size(0, 0));
+    const translateX = useSharedValue(0);
+    const translateY = useSharedValue(0);
+    const scale = useSharedValue(1.0);
+    const startX = useSharedValue(0);
+    const startY = useSharedValue(0);
+    const startScale = useSharedValue(0);
+    const { theme, toggleTheme } = useContext(Theme.context)
+    const styles = makeStyleSheet(theme)
+    const [viewSize, setViewSize] = useState(Size(0, 0))
+    const [isShowMetaData, setIsShowMetaData] = useState(false);
+    const [selectedIndex, setSelectedIndex] = useState(null);
 
+    useEffect(() => {
+      if (selectedIndex == null) {
+        return
+      }
+      console.log("Container: selectedIndex useEffect", selectedIndex, "metaDataViewRef:", metaDataViewRef)
+      metaDataViewRef?.current?.selectIndex(selectedIndex)
+    })
 
-  useEffect(() => {
-    if (imageInfo) {
-      Image.getSize(imageInfo.uri, (width, height) => {
-        setImageSize(Size(width, height));
-      }, error => {
-        // handle error
-      })
-    }
-  }, [imageInfo]);
+    useImperativeHandle(ref, () => (
+      {
+        imageInfo: imageInfo,
+        selectIndex: (index) => {
+          console.log()
+          setIsShowMetaData(true)
+          setSelectedIndex(index)
+        }
+      }
+    ));
 
-  useEffect(() => {
-    fitInContainer()
-  }, [imageSize]);
+    useEffect(() => {
+      if (imageInfo) {
+        Image.getSize(imageInfo.uri, (width, height) => {
+          setImageSize(Size(width, height));
+        }, error => {
+          // handle error
+        })
+      }
+    }, [imageInfo]);
 
-  const gestureStyle = useAnimatedStyle(() => (
-    {
-      transform: [
-        { scale: scale.value },
-        { translateX: translateX.value },
-        { translateY: translateY.value }
-      ]
-    }
-  ))
-
-  const fitInContainer = (newScale = null) => {
-    if (!viewSize.width || !viewSize.height) {
-      return 1.0
-    }
-    newScale = newScale ?? viewSize.height / imageSize.height
-    if (imageSize.width * newScale > viewSize.width) {
-      newScale *= viewSize.width / (imageSize.width * newScale)
-    }
-    recenterWithScale(newScale)
-  }
-
-  const recenterWithScale = (newScale = null) => {
-    translateX.value = 0.0
-    translateY.value = 0.0
-    if (newScale) {
-      scale.value = newScale
-    }
-  }
-
-  const toggleScale = () => {
-    if (!(imageSize) || !(viewSize)) {
-      return
-    }
-    let newScale = scale.value
-    if (newScale === 1.0) {
+    useEffect(() => {
       fitInContainer()
-    } else {
-      recenterWithScale(1.0)
-    }
-  }
+    }, [imageSize]);
 
-  const tapGestureHandler = Gesture.Tap()
-    .numberOfTaps(2)
-    .onEnd(() => {
-      runOnJS(toggleScale)();
+    const gestureStyle = useAnimatedStyle(() => (
+      {
+        transform: [
+          { scale: scale.value },
+          { translateX: translateX.value },
+          { translateY: translateY.value }
+        ]
+      }
+    ))
+
+    const fitInContainer = (newScale = null) => {
+      if (!viewSize.width || !viewSize.height) {
+        return 1.0
+      }
+      newScale = newScale ?? viewSize.height / imageSize.height
+      if (imageSize.width * newScale > viewSize.width) {
+        newScale *= viewSize.width / (imageSize.width * newScale)
+      }
+      recenterWithScale(newScale)
+    }
+
+    const recenterWithScale = (newScale = null) => {
+      translateX.value = 0.0
+      translateY.value = 0.0
+      if (newScale) {
+        scale.value = newScale
+      }
+    }
+
+    const toggleScale = () => {
+      if (!(imageSize) || !(viewSize)) {
+        return
+      }
+      let newScale = scale.value
+      if (newScale === 1.0) {
+        fitInContainer()
+      } else {
+        recenterWithScale(1.0)
+      }
+    }
+
+    const tapGestureHandler = Gesture.Tap()
+      .numberOfTaps(2)
+      .onEnd(() => {
+        runOnJS(toggleScale)();
+      });
+
+    const panGestureHandler = Gesture.Pan().onStart((event) => {
+      startX.value = translateX.value
+      startY.value = translateY.value
+    }).onUpdate((event) => {
+      translateX.value = startX.value + event.translationX
+      translateY.value = startY.value + event.translationY
     });
 
-  const panGestureHandler = Gesture.Pan().onStart((event) => {
-    startX.value = translateX.value
-    startY.value = translateY.value
-  }).onUpdate((event) => {
-    translateX.value = startX.value + event.translationX
-    translateY.value = startY.value + event.translationY
-  });
+    const pinchGestureHandler = Gesture.Pinch().onStart((event) => {
+      startScale.current = scale.value
+    }).onUpdate((event) => {
+      let newScale = startScale.current * event.scale
+      if (newScale < 0.2) {
+        newScale = 0.2
+      }
+      scale.value = newScale
+    })
 
-  const pinchGestureHandler = Gesture.Pinch().onStart((event) => {
-    startScale.current = scale.value
-  }).onUpdate((event) => {
-    let newScale = startScale.current * event.scale
-    if (newScale < 0.2) {
-      newScale = 0.2
-    }
-    scale.value = newScale
-  })
+    const combinedGestureHandlers = Gesture.Simultaneous(
+      panGestureHandler,
+      tapGestureHandler,
+      pinchGestureHandler
+    )
 
-  const combinedGestureHandlers = Gesture.Simultaneous(
-    panGestureHandler,
-    tapGestureHandler,
-    pinchGestureHandler
-  )
+    const toolbarConfig = ToolbarConfig({
+      onPressAddPictureButtonListener: onPressAddPictureButton,
+      isAddPictureButtonEnabled: true,
+      onImageInfoButtonListener: () => {
+        setIsShowMetaData(!isShowMetaData)
+      }
+      ,
+      isImageInfoButtonEnabled: imageInfo == null ? false : true
+    });
 
-  const toolbarConfig = ToolbarConfig({
-    onPressAddPictureButtonListener: onPressAddPictureButton,
-    isAddPictureButtonEnabled: true,
-    onImageInfoButtonListener: () => {
-      setIsShowMetaData(!isShowMetaData)
-    }
-    ,
-    isImageInfoButtonEnabled: imageInfo == null ? false : true
-  });
-
-  console.log("toolbarConfig", JSON.stringify(toolbarConfig))
-  return (
-    <View style={[styles.containerView]}>
-      <Toolbar config={toolbarConfig} />
-      <View style={{ flex: 1 }}>
-        <GestureHandlerRootView style={
-          [
-            { flex: 1 }
-          ]
-        }>
-          <GestureDetector gesture={combinedGestureHandlers}>
-            <Animated.View
-              style={[
-                {
-                  flex: 1, justifyContent: 'center', alignItems: 'center',
-                  overflow: "hidden"
-                }
-              ]}
-              onLayout={(event) => {
-                const { width, height } = event.nativeEvent.layout;
-                setViewSize({ width, height })
-              }}>
-              <AnimatedImage
-                source={{ uri: imageInfo?.uri }}
-                style={[{
-                  height: imageSize.height,
-                  width: imageSize.width
-                }, gestureStyle]}
-                resizeMode="contain"
-              />
-            </Animated.View>
-          </GestureDetector>
-        </GestureHandlerRootView>
-        {
-          isShowMetaData &&
-          <Overlay>
-            <MetaDataView style={{
-              flex: 1,
-              backgroundColor: 'red'
-            }}
-              metaData={imageInfo?.metaData}
-            >
-            </MetaDataView>
-          </Overlay>
-        }
+    return (
+      <View style={[styles.containerView]}>
+        <Toolbar config={toolbarConfig} />
+        <View style={{ flex: 1 }}>
+          <GestureHandlerRootView style={
+            [
+              { flex: 1 }
+            ]
+          }>
+            <GestureDetector gesture={combinedGestureHandlers}>
+              <Animated.View
+                style={[
+                  {
+                    flex: 1, justifyContent: 'center', alignItems: 'center',
+                    overflow: "hidden"
+                  }
+                ]}
+                onLayout={(event) => {
+                  const { width, height } = event.nativeEvent.layout;
+                  setViewSize({ width, height })
+                }}>
+                <AnimatedImage
+                  source={{ uri: imageInfo?.uri }}
+                  style={[{
+                    height: imageSize.height,
+                    width: imageSize.width
+                  }, gestureStyle]}
+                  resizeMode="contain"
+                />
+              </Animated.View>
+            </GestureDetector>
+          </GestureHandlerRootView>
+          {
+            isShowMetaData &&
+            <Overlay>
+              <MetaDataView
+                ref={metaDataViewRef}
+                style={{
+                  flex: 1,
+                  backgroundColor: 'red'
+                }}
+                metaData={imageInfo?.metaData}
+                onSelectMetaDataItemHandler={(selectedMetaDataItem) => {
+                  onSelectMetaDataItemHandler(ref, selectedMetaDataItem)
+                }}
+              >
+              </MetaDataView>
+            </Overlay>
+          }
+        </View>
       </View>
-    </View>
-  );
-}
+    );
+  })
 
 const makeStyleSheet = (theme) => {
   return StyleSheet.create(
--- src/common/components/MetaDataView.js
+++ src/common/components/MetaDataView.js
@@ -1,32 +1,40 @@
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { MetaDataTableView } from "./MetaDataTable/MetaDataTableView";
-import { useContext, useState } from "react";
+import { forwardRef, useContext, useImperativeHandle, useRef, useState } from "react";
 import { StyleSheet, View } from "react-native";
 import Theme from "../../Theme";
 import { useHeaderHeight } from '@react-navigation/elements';
 import * as Utils from "../utilities/Utils";
 
-const MetaDataView = ({ style, metaData }) => {
+const MetaDataView =  forwardRef(({ style, metaData, onSelectMetaDataItemHandler }, ref) => {
     const { theme, toggleTheme } = useContext(Theme.context)
     const [selectedItems, setSelectedItems] = useState([])
     const headerHeight = useHeaderHeight();
     const styles = makeStyleSheet(theme, headerHeight)
     const metaDataCellModelArray = Utils.makeMetaDataCellModelArrayFromMetaData(metaData, selectedItems)
-
-    const handleSelectMetaDataItem = (metaDataItem) => {
+    const tableViewRef = useRef(null);
+    useImperativeHandle(ref, () => ({
+        selectIndex: (index) => {
+            console.log("MetaDataView: selectIndex: ", index)
+            tableViewRef.current.selectIndex(index)
+        }
+    }))
+    const handleOnSelectMetaDataItem = (metaDataItem) => {
         setSelectedItems([metaDataItem])
+        onSelectMetaDataItemHandler(metaDataItem)
     }
 
     return (
         <View style={[style, styles.container]}>
             <MetaDataTableView
+                ref={tableViewRef}
                 style={{ flex: 1 }}
                 metaDataCellModelArray={metaDataCellModelArray}
-                onSelectMetaData={handleSelectMetaDataItem}
+                onSelectMetaDataItemHandler={handleOnSelectMetaDataItem}
             />
         </View>
     )
-}
+});
 
 const makeStyleSheet = (theme, headerHeight) => {
     return StyleSheet.create({
