d478ad17f445b55f4942f643f2196beae7b6f2ba
vijay <vjc@gmail.com>
2024-12-05 22:38:43 +0800

    deleted MetaDataTableView (FlatList in MetaDataView)

--- src/common/components/MetaDataView.js
+++ src/common/components/MetaDataView.js
@@ -1,12 +1,11 @@
-import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
-import { MetaDataTableView } from "./MetaDataTable/MetaDataTableView";
 import { forwardRef, useContext, useEffect, useImperativeHandle, useRef, useState } from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, FlatList } from "react-native";
 import Theme from "../../Theme";
 import { useHeaderHeight } from '@react-navigation/elements';
+import MetaDataCell from "./MetaDataTable/MetaDataCell";
 import * as Utils from "../utilities/Utils";
 
-const MetaDataView =  forwardRef(({ style, metaData, onSelectMetaDataItemHandler }, ref) => {
+const MetaDataView = forwardRef(({ style, metaData, onSelectMetaDataItemHandler }, ref) => {
     const { theme, toggleTheme } = useContext(Theme.context)
     const [selectedItems, setSelectedItems] = useState([])
     const headerHeight = useHeaderHeight();
@@ -14,19 +13,19 @@ const MetaDataView =  forwardRef(({ style, metaData, onSelectMetaDataItemHandler
     const metaDataCellModelArray = metaData == null ? [] : Utils.makeMetaDataCellModelArrayFromMetaData(
         metaData, selectedItems
     );
-     
-    const tableViewRef = useRef(null);
-    
+
+    const flatListRef = useRef(null);
+
     useImperativeHandle(ref, () => ({
         selectIndex: (selectedIndex) => {
             if (selectedIndex >= metaDataCellModelArray.length) {
                 return
             }
-            
+
             const selectedMetaDataItem = metaDataCellModelArray[selectedIndex]?.metaDataItem
             setSelectedItems(selectedMetaDataItem ? [selectedMetaDataItem] : [])
-            tableViewRef?.current?.scrollToIndex(index); 
-    }
+            // flatListRef?.current?.scrollToIndex(selectedIndex);
+        }
     }))
 
     const handleOnSelectMetaDataItem = (metaDataItem) => {
@@ -35,11 +34,19 @@ const MetaDataView =  forwardRef(({ style, metaData, onSelectMetaDataItemHandler
     }
     return (
         <View style={[style, styles.container]}>
-            <MetaDataTableView
-                ref={tableViewRef}
-                style={{ flex: 1 }}
-                metaDataCellModelArray={metaDataCellModelArray}
-                onSelectMetaDataItemHandler={handleOnSelectMetaDataItem}
+            <FlatList
+                ref={flatListRef}
+                data={metaDataCellModelArray}
+                keyExtractor={(dataItem) => dataItem.metaDataItem.title}
+                renderItem={
+                    ({ item: model }) => {
+                        return <MetaDataCell
+                            model={model}
+                            onSelectMetaDataItemHandler={handleOnSelectMetaDataItem}
+
+                        />
+                    }
+                }
             />
         </View>
     )
